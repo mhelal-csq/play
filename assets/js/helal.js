5e21f3c3992bdfabfcb663b502f9b7a10a5ff11e
Mohamed Helal <mohamed.helal@contentsquare.com>
2025-01-21 15:31:53 +0200

    add log

--- assets/js/helal.js
+++ assets/js/helal.js
@@ -21,6 +21,6 @@ form.addEventListener("submit", (event) => {
     // Log the event to the console
     console.log(`Tracked event with carType: ${carType}`);
     carTypeInput = '';
-    CSSConditionRule.log('Car Input set to null')
+    console.log('Car Input set to null')
     
 } )
